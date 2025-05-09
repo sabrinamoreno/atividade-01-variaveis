8ea106159b9ff56a745cba018d68169a49541f22
Sabrina <sabrinamorenorodr28741@gmail.com>
2025-05-09 20:24:33 -0300

    Add files via upload

---
+++ Atividade_mentoria/script.js
@@ -0,0 +1,49 @@
+// Crie três variáveis para armazenar:
+
+//- Seu **nome** (`string`)
+//- Sua **idade** (número)
+//- Sua **cidade** (`string`)
+
+//Após declarar as variáveis, utilize **`console.log()`** para exibir cada uma separadamente.
+
+let nome = 'Sabrina';
+let idade= 22;
+let cidade= 'São Luís';
+
+console.log('Resposta 1 abaixo:');
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+
+//Agora, crie duas variáveis:
+
+//- Uma para armazenar **seu nome**
+//- Outra para armazenar **seu sobrenome**
+
+//Depois, **exiba ambos juntos em um único `console.log()`**, utilizando **concatenação**.
+
+
+console.log('Resposta 2 abaixo:');
+
+let primeiroNome= 'Sabrina';
+let sobrenome= 'Rodrigues';
+
+console.log(primeiroNome +" "+ sobrenome);
+
+
+// Crie duas variáveis:
+
+//- Uma para armazenar **seu nome**
+//- Outra para armazenar **seu hobby**
+
+//Utilize **concatenação** ou **`template literals` (`${}`)** para exibir a seguinte frase no console:
+//*"Eu sou [seu nome] e meu hobby é [seu hobby]."*
+
+console.log('Resposta 3 abaixo:');
+
+let meuNome='Sabrina';
+let hobby='música';
+
+console.log(`Eu sou ${meuNome} e meu hobby é ${hobby}`);
